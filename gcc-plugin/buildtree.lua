87aa871db8f5bf88b0c4ab842811db87d4333702
stephen010x <stephen010x@gmail.com>
2025-12-08 23:46:24 -0700

    backup

--- gcc-plugin/buildtree.lua
+++ gcc-plugin/buildtree.lua
@@ -7,17 +7,66 @@ local module = {}
 
 
 
+debug = {}
+
+debug.string_table = function(t, is_deep, sep, tabspace, _pref)
+    if is_deep == nil then is_deep = false end
+    --if sep == nil then sep = ' ' end
+    --if tabspace == nil then tabspace = '' end
+    if sep == nil then sep = '\n' end
+    if tabspace == nil then tabspace = '   ' end
+    if _pref == nil then _pref = '' end
+
+    local _tstr = {}
+    table.insert(_tstr, '{')
+    for key, value in pairs(t) do
+        local vstr
+
+        if type(value) == "table" and is_deep then
+            if next(value) == nil then
+                vstr = '{}'
+            else
+                vstr = debug.string_table(value, is_deep, sep, tabspace, _pref..tabspace)
+            end
+        elseif type(value) == "string" then 
+            vstr = '"'..tostring(value)..'"'
+        else 
+            vstr = tostring(value)
+        end
+
+        table.insert(_tstr, ('%s[%s] = %s,'):format(_pref..tabspace, tostring(key), vstr))
+
+    end
+    table.insert(_tstr, _pref..'}')
+
+    return table.concat(_tstr, sep)
+end
+
+
+debug.print_table = function(t, is_deep, sep, tabspace)
+    print(debug.string_table(t, is_deep, sep, tabspace))
+end
+
+module.debug = debug
+
+
+-- debug.print_table(table, true, '\n', '\t')
+
+
+
+
+
 local enum = {
     meta = {
         __index = function(self, key)
             return key
-        end
+        end,
         __newindex = function(table, key, value)
             print("writing to ext table not allowed")
-        end
+        end,
     },
 }
-setmetatable(ext, ext.meta)
+setmetatable(enum, enum.meta)
 
 module.enum = enum
 module.ext  = enum
@@ -29,16 +78,17 @@ module.type = enum
 
 
 
-local function cmd(command)
+local function cmd(command, is_print)
+    if is_print then print(command) end
     local handle = io.popen(command)
     local out = handle:read("*a")
     local ok, stat = handle:close()
+    -- remove trailing newline if it exists
+    if out:sub(-1) == '\n' then out = out:sub(1, -2) end
     return out
 end
 
 module.cmd = cmd
-
-
 
 
 
@@ -51,17 +101,18 @@ stringmt.__index.split = function(self, sep)
     if sep == nil then sep = " " end
     if sep == "" then return self end
 
-    local out = table.new()
+    --local out = table.new()
+    local out = {}
     local index = 1
     local last = 1
     while true do
         start, stop = self:find(sep, index, true)
         if not start then break end
-        out:insert(self:sub(last, start-1))
+        table.insert(out, self:sub(last, start-1))
         index = stop + 1
         last = index
     end
-    out:insert(self:sub(last))
+    table.insert(out, self:sub(last))
 
     if strip == true then
         for key, value in pairs(out) do
@@ -72,10 +123,17 @@ stringmt.__index.split = function(self, sep)
     return out
 end
 
--- strip string of whitespace
+-- strip beginning and end of string of whitespace
 -- adds method to main string metatable
 stringmt.__index.strip = function(self)
     return self:gsub("^%s+", ""):gsub("%s+$", "")
+end
+
+-- replaces all consecutive whitespace with single spaces
+-- adds method to main string metatable
+stringmt.__index.squeeze = function(self)
+    local s = self:gsub("%s+", " ")
+    return s:gsub("^%s+", ""):gsub("%s+$", "")
 end
 
 
@@ -88,7 +146,7 @@ end
 
 table.copy = function(t)
     rt = {}
-    for key, value in pairs do
+    for key, value in pairs(t) do
         rt[key] = value
     end
     return rt
@@ -98,6 +156,8 @@ table.new = function(rtable)
     if rtable == nil then rtable = {} end
     return setmetatable(table.copy(rtable), table)
 end
+
+table.print = debug.print_table
 
 -- implement later
 -- table.slice = function(t, first, last)
@@ -109,22 +169,35 @@ end
 
 
 
-
+local fs
 
 -- linux file system helper
-local fs = {
+fs = {
     ls = function(path)
-        if path == nil then path = "."
-        local out = cmd("ls \""..path.."\""):split(' ', true)
+        if path == nil then path = "." end
+        local files = cmd("ls -1 --color=never \""..path.."\""):split('\n', true)
+        for key, value in pairs(files) do
+            files[key] = './'..fs.get_nearpath(path..'/'..value)
+        end
+        return files
     end,
     
     cd = function(path)
-        if path == nil then path = "~"
+        if path == nil then path = "~" end
         return cmd("cd \""..path.."\"")
     end,
 
+    -- rm = function(path, opts)
+    --     cmd("\\rm "..opts.." \""..path.."\"")
+    -- end,
+
     pwd = function()
         return cmd("pwd -P")
+    end,
+
+    mkdir = function(path, opts)
+        if opts == nil then opts = "" end
+        cmd ("mkdir "..opts.." "..path)
     end,
 
     get_type = function(path)
@@ -132,14 +205,20 @@ local fs = {
     end,
 
     get_fullpath = function(path)
-        return cmd("realpath \""..path.."\"")
+        return cmd("realpath --canonicalize-missing \""..path.."\"")
+    end,
+
+    get_dir = function(path)
+        return cmd("dirname -- "..path)
     end,
 
     -- get file extension from name
     get_ext = function(path)
+        --if path == nil then return nil end
         local index = path:reverse():find('.', 1, true)
-        index = #path - index + 2
-        return i and path:sub(index) or ""
+        if index == nil then return nil end
+        index = #path - index + 1
+        return index and path:sub(index) or ""
     end,
 
     get_name = function(path)
@@ -147,31 +226,36 @@ local fs = {
     end,
 
     set_ext = function(names, ext)
-        if type(names) ~= table then names = {names} end
+        local is_table = type(names) == "table"
+        if not is_table then names = {names} end
 
         local rt = {}
 
         for key, name in pairs(names) do
-            name = name:split('.')[1]
+            local nlist = name:split('.')
+            name = nlist[#nlist-1]
             rt[key] = name..'.'..ext
         end
+
+        return is_table and rt or select(2, next(rt))
     end,
 
     -- returns path relative to base
-    get_nearpath = function(base, path)
+    get_nearpath = function(path, base)
+        if base == nil then base = '.' end
         return cmd("realpath --relative-to=\""..base.."\" \""..path.."\"")
     end,
 
     get_time = function(path)
-        return math.tointeger(tonumber(cmd("stat -c %Y "..path)))
-    end
+        return tonumber(cmd("stat -c %Y "..path.." 2>/dev/null"))
+    end,
 
     is_dir = function(path)
-        return cmd("[ -d "..path.." ] && echo 1") == "1"
+        return cmd("[ -d \""..path.."\" ] && echo 1") == "1"
     end,
 
     is_exist = function(path)
-        return cmd("[ -e "..path.." ] && echo 1") == "1"
+        return cmd("[ -e \""..path.."\" ] && echo 1") == "1"
     end,
 
     --is_ext = function(path, ext) end
@@ -179,28 +263,72 @@ local fs = {
 
 
 
-fs.find = function(path, is_recursive, filter)
-    if is_recursive == nil then is_recursive = false
+
+-- fs.find = function(path, is_recursive, filter)
+--     if is_recursive == nil then is_recursive = false end
+-- 
+--     if not fs.is_dir(path) then
+--         print("fs.find(...): \""..path.."\" is not a directory")
+--     end
+--         
+--     --local dirs = table.new({path})
+--     local dirs = {path}
+--     local newdirs = {}
+--     local files = {}
+--     
+--     while #dirs ~= 0 do
+--         local _, dpath = next(dirs)
+--         if dpath == nil then 
+--             if #newdirs == 0 then break end
+--             
+--         end
+-- 
+--         table.insert(newdirs, dpath)
+-- 
+--         print(#dirs)
+--         
+--         for _, fpath in pairs(fs.ls(dpath)) do
+--             print(fpath)
+--             print(fs.is_dir(fpath))
+--             if fs.is_dir(fpath) then
+--                 table.insert(dirs, fpath)
+--             elseif filter(fs.get_ext(fpath)) then
+--                 table.insert(files, fpath)
+--             end
+--         end
+--         
+--     end
+-- 
+--     return files
+-- end
+
+
+
+fs.find = function(path, is_recursive, filter, _files)
+    if is_recursive == nil then is_recursive = false end
+    if _files == nil then _files = {} end
 
     if not fs.is_dir(path) then
         print("fs.find(...): \""..path.."\" is not a directory")
-        
-    local dirs = table.new({path})
-    local files = table.new()
-
-    while #dirs ~= 0 do
-        local _, dpath = next(dirs)
-        if dpath == nil then break end
-        
-        for _, fpath in pairs(fs.ls(dpath)) do
-            if fs.is_dir(fpath) then
-                dirs:insert(fpath)
-            elseif filter(fs.get_ext(fpath)) then
-                files:insert(fpath)
-            end
-        end
+        return false
     end
+
+    local files = fs.ls(path)
+
+    for _, file in pairs(files) do
+    
+        if fs.is_dir(file) then
+            fs.find(file, true, filter, _files)
+        elseif filter(file) then
+            table.insert(_files, file)
+        end
+        
+    end
+
+    return _files
 end
+
+
 
 module.fs = fs
 
@@ -239,17 +367,15 @@ module.newtree = function()
         targets = setmetatable({}, {
         
             __index = function(self, key)
-                return rawget(self, fs.fullpath(key))
-            end
+                return rawget(self, fs.get_fullpath(key))
+            end,
 
             __newindex = function(self, key, value)
-                rawset(self, fs.fullpath(key), value)
-            end
+                rawset(self, fs.get_fullpath(key), value)
+            end,
             
         }),
 
-
-        local should_compile = false
 
 
         -- this is a recursive function
@@ -264,7 +390,7 @@ module.newtree = function()
             if ttarg == nil then return true end
         
             -- get time of target
-            local targ_time = fs.get_time(target)
+            local targ_time = fs.get_time(target) or 0
 
             local should_rebuild = false
             
@@ -275,8 +401,9 @@ module.newtree = function()
             for _, path in pairs(ttarg.depends) do
                 -- check if it exists in the buildtree
                 -- if so then call self:build on it
-                if self.buildtree.targets[path] ~= nil then
-                    self:build(path)
+                if self.targets[path] ~= nil then
+                    local ok = self:build(path)
+                    if not ok then return ok end
                 end
 
                 -- check if dependancy exists in the filesystem
@@ -285,15 +412,16 @@ module.newtree = function()
 
                 -- check if dep is newer than targ
                 -- if newer, then set should_rebuild flag
-                if fs.get_time(path) >= targ_time then
+                if (fs.get_time(path) or 0) >= targ_time then
                     should_rebuild = true
                 end
             end
 
             if should_rebuild then
-                ttarg.builder(ttarg.depends, ttarg.options)
+                ttarg.builder(target, ttarg.depends, ttarg.options)
             end
-            
+
+            return true
         end,
     }
 end
@@ -306,18 +434,34 @@ local gcc = {}
 
 -- return header dependancies
 gcc.gen_dep = function(file, opts)
-    local str = cmd("gcc "..file.." -MM "..opts)
+    local str = cmd("gcc "..file.." -MM "..opts, true)
     str = str:split(":")[2]
-    str = str:gsub("\n", " ", true)
-    local deps = str:split()
+    str = str:gsub("\n", " ")
+    str = str:gsub("\\", " ")
+    local deps = str:squeeze():split()
     -- get rid of first source file
-    return table.move(deps, 2, 1, #deps-1)
+    --return table.concat(table.move(deps, 2, 1, #deps-1), ' ')
+    return deps
+end
+
+
+gcc.filter_headers = function(files)
+    local rt = {}
+    for _, file in pairs(files) do
+        if fs.get_ext(file) ~= '.h' then
+            table.insert(rt, file)
+        end
+    end
+    return rt
 end
 
 
 gcc.builder = function(outpath, infiles, opts)
-    local infiles_str = table.concat(infiles, ' ')
-    cmd("gcc -o %s %s %s":format(outpath, infiles_str, opts))
+    -- make sure output directory exists
+    fs.mkdir(fs.get_dir(outpath), "-p")
+    -- compile
+    local infiles_str = table.concat(gcc.filter_headers(infiles), ' ')
+    cmd(("gcc -o %s %s %s"):format(outpath, infiles_str, opts), true)
 end
 
 
--- gcc-plugin/make.lua
+++ gcc-plugin/make.lua
@@ -1,70 +1,171 @@
+#!/usr/bin/env lua
+-- make.lua [goal]
+-- arg[0] = script name, arg[1]..arg[n] = args, #arg = count
 local build = require "buildtree"
 local fs = build.fs
+local cmd = build.cmd
 local filter = build.filter
 local enum, ext, type = build.enum, build.ext, build.type
-local gcc = build.gcc
+local cc = build.gcc
+local debug = build.debug
 
+
+local system = enum.linux
 
 local dir = {
     src = "src",
     tmp = "tmp",
     inc = "inc",
     bin = "bin",
+    lib = "lib",
 }
-
-local target = "main.exe"
-local srcdir = "src"
-local bindir = "bin"
-
-local flags = "-d -Os"
-local cflags = "-DDEBUG"
-local lflags = " -fPIC"
-
-
-cflags = cflags.." -I"..dir.inc
 
 
 -- get location of gcc plugin headers
 local gcc_plugin_dir = cmd("gcc -print-file-name=plugin")
-cflags = cflags.." -I"..gcc_plugin_dir.."/include"
+
+
+flags = "-Wall -Wextra"
+cflags = "-std=gnu17 -fvisibility=internal -I" .. dir.inc
+cflags = cflags .. " -isystem" .. gcc_plugin_dir .. "/include"
+lflags = ""
+
+
+-- these are the flags actually used, so make sure to append the previous shared flags
+local goals = {
+    debug = {
+        flags  = "-g -Og " .. flags,
+        cflags = "-D__DEBUG__ -D__debug__ " .. cflags,
+        lflags = "" .. lflags,
+    },
+    release = {
+        flags  = "-Os -g0" .. flags,
+        cflags = "" .. cflags,
+        lflags = "-s" .. lflags,
+    },
+}
+
+goals.default = goals.debug
+
+
+local outputs = {
+    cmd = {
+        target = "bin/hyper",
+        srcdir = "src/cmd",
+    },
+    lib = {
+        target = "bin/hyper.so",
+        srcdir = "src/lib",
+        cflags = "-pie -Wl,-E",
+    },
+}
 
 
 
--- get recursive list of C files in the source directory
-local c_srcs = fs.find(dir.src, true, filter.ext(ext.c))
+
+
+
+local function concat_flags(...)
+    flist = {...}
+    out = ''
+    for _, flags in pairs(flist) do
+        out = out..(' '..flags or '')
+    end
+    return out:squeeze()
+end
+
+
+local goal = arg[1] or 'default'
+
+-- if goal == "clean" then
+--     fs.rm(dir.tmp, "-rf")
+-- elseif goal == "test" then
+-- elseif goal == "debug" then
+-- elseif goal == "dbg" then
+-- end
+
+-- for gname, g in pairs(goals) do
+--     if gname == goal then
+--         flags  = concat_flags(flags,  g.flags)
+--         cflags = concat_flags(cflags, g.cflags)
+--         lflags = concat_flags(lflags, g.lflags)
+--     end
+-- end
+
+
+
+
 
 
 
 local buildtree = build.newtree()
 
 
--- add target to build tree
-buildtree.targets[dir.bin..'/'..target] = {
-    type = type.elf,
-    builder = gcc.bulder,
-    depends = c_objs,
-    options = flags.." "..lflags,
-    force_rebuild = false,
-}
 
 
+local function gen_objs(srcs, tmpdir, refdir, opts)
+    if refdir == nil then refdir = fs.pwd() end
+    
+    local objs = {}
 
--- generate dependancy tree from sources
-for _, cfile in pairs(c_srcs) do
-    local deps = gcc.gen_dep(cfile, options)
+    -- generate dependancy tree from sources
+    for _, cfile in pairs(srcs) do
 
-    local objfile = fs.set_ext(cfile, ext.o)
+        local nearpath = fs.get_nearpath(cfile, refdir)
+        local objfile = fs.set_ext(tmpdir..'/'..nearpath, ext.o)
 
-    buildtree.targets[objfile] = {
-        type = ext.o,
-        builder = gcc.builder,
-        depends = gcc.gen_dep(cfile, cflags),
-        options = cflags.." -c",
+        print(objfile)
+        
+        table.insert(objs, objfile)
+    
+        buildtree.targets[objfile] = {
+            --type = ext.o,
+            builder = cc.builder,
+            depends = cc.gen_dep(cfile, opts),
+            options = opts.." -c",
+            force_rebuild = false,
+        }
+    end
+
+    return objs
+end
+
+
+-- add targets to build tree
+for _, out in pairs(outputs) do
+    -- get recursive list of C files in the source directory
+    local c_srcs = fs.find(out.srcdir, true, filter.ext('.c'))
+
+    -- local oflags = concat_flags(flags, cflags, out.flags, out.cflags)
+    -- local xflags = concat_flags(flags, lflags, out.flags, out.lfla
+    local oflags = concat_flags(goals[goal].flags, goals[goal].cflags, out.flags, out.cflags)
+    local xflags = concat_flags(goals[goal].flags, goals[goal].lflags, out.flags, out.lflags)
+    
+    -- generate objects into buildtree
+    local tmpdir = dir.tmp .. '/' .. out.target .. '_' .. goal
+    local c_objs = gen_objs(c_srcs, tmpdir, fs.pwd(), oflags)
+
+    buildtree.targets[out.target] = {
+        --type = type.elf,
+        builder = cc.builder,
+        depends = c_objs,
+        options = xflags,
         force_rebuild = false,
     }
 end
 
 
 
--- build target
-buildtree:build(target)
+debug.print_table(buildtree, true, '\n', '    ')
+
+
+
+-- build outputs
+-- TODO: add a way to interrupt this if there is an error
+for _, out in pairs(outputs) do
+    local ok = buildtree:build(out.target)
+    if not ok then 
+        print("build failed")
+        break
+    end
+end
